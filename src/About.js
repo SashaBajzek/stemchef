dad4ba0a84b1a5f94dfbf78d391ca85e773524c1
Sasha Bajzek <dabridginator@gmail.com>
2017-08-08 22:44:20 -0700

    Set up SASS

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './styles/About.css';
+import './styles/About.scss';
 
 class About extends Component {
   render() {
