dcb329926812c0b2b9dbf22c6a6a005bcf3a83fb
Sasha Bajzek <dabridginator@gmail.com>
2017-08-15 06:04:00 -0700

    Fix header and remove footer nav

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './styles/Footer.scss';
-import { NavLink } from 'react-router-dom';
 
 class Footer extends Component {
 	year = new Date().getFullYear();
@@ -8,16 +7,6 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-				<div className="Footer__container">
-					<div className="Footer__nav">
-						<NavLink to="/" className="Footer__logo"><div className="Footer__logo-image"></div><h1 className="Footer__logo-text">StemChef</h1></NavLink>
-						<ul className="Footer__menu">
-							<li><NavLink to="/programs" className="Footer__navItem" activeClassName="Footer__navItem--selected">PROGRAMS</NavLink></li>
-							<li><NavLink to="/our-story" className="Footer__navItem" activeClassName="Footer__navItem--selected">OUR STORY</NavLink></li>
-							<li><NavLink to="/contact" className="Footer__navItem" activeClassName="Footer__navItem--selected">CONTACT</NavLink></li>
-						</ul>
-					</div>
-				</div>
 				<small className="Footer__copyright">&copy; {this.year} StemChef</small>
       </div>
     );
