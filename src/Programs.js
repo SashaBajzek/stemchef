fea862cfe67bc718ea5c860a9aa5cf5dc9997383
Sasha Bajzek <dabridginator@gmail.com>
2017-08-15 06:35:49 -0700

    Style program page

--- src/Programs.js
+++ src/Programs.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import AfterSchool from './AfterSchool';
 import './styles/Programs.scss';
 
 class Programs extends Component {
 	render() {
 		return (
 			<div className="Programs">
-				<div className="Programs__container">
-					<h1>Program</h1>
-					<section>
-						<div className="Programs__image"></div>
-						<p>We have delicious programs in store for our young scientists. Young chefs will explore questions in chemistry, physics, biology, architecture and more as they solve yummy challenges mathematically while whipping up delicious edible experiments.</p>
-					</section>
-					<AfterSchool />
+				<div className="Programs__hero">
+					<div className="Programs__modal">
+						<h1>Program</h1>
+						<section>
+							<div className="Programs__image"></div>
+							<p>We have delicious programs in store for our young scientists. Young chefs will explore questions in chemistry, physics, biology, architecture and more as they solve yummy challenges mathematically while whipping up delicious edible experiments.</p>
+						</section>
+					</div>
 				</div>
 				{this.props.children}
 			</div>
